25521e2fb025e5f12f58620c7c92191265369c1e
faker01 <s.maksimenko2004@mail.ru>
2024-06-30 15:32:14 +0200

    0.9

--- take_start_directory.py
+++ take_start_directory.py
@@ -1,8 +1,15 @@
 import os
 
+
+def find_path(lst):
+    for i in range(len(lst)):
+        if "tg_alert_bot" in lst[i]:
+            return i
+
+
 def start_directory():
     path = os.getcwd()
     path = path.split("\\")
-    path = path[:path.index("tg_alert_bot") + 1]
+    path = path[:find_path(path) + 1]
     path = "\\".join(path)
     os.chdir(path)
